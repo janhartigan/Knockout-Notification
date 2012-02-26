a31fcf00c7206e780b63417e20a02e219258a131
janhartigan <janhartigan13@gmail.com>
2012-02-26 20:28:00 +0000

    'Updating to v1.1.0 with the bug fix adding the callback function'

--- KnockoutNotification.knockout.js
+++ KnockoutNotification.knockout.js
@@ -1,18 +1,19 @@
 /**
- * Knockout Notification plugin v1.0.0
+ * Knockout Notification plugin v1.1.0
  * (c) 2011 Jan Hartigan - http://www.janhartigan.com
  * License: dual (MIT, GPL)
  */
 ko.bindingHandlers.notification = {
 	update: function(element, valueAccessor, allBindingsAccessor, viewModel) {
 		var rawValue = valueAccessor(),
-			//notification can be passed an object with properties 'message', 'duration', and 'callback', or it can be given just a string
+			//notification can be passed an object with properties 'message', 'duration', 'fadeoutDuration', 'hide', 'fade', and 'callback', or it can be given just a string
 			options = typeof rawValue == 'object' ? rawValue : {message: rawValue},
 			message = ko.utils.unwrapObservable(options.message),
 			duration = options.duration !== undefined ? ko.utils.unwrapObservable(options.duration) : 5000, //5 seconds is default fade out
 			fadeoutDuration = options.fadeoutDuration !== undefined ? ko.utils.unwrapObservable(options.fadeoutDuration) : 200, //default is 200 ms
 			hide = options.hide !== undefined ? ko.utils.unwrapObservable(options.hide) : true, //default is to hide it
 			fade = options.fade !== undefined ? ko.utils.unwrapObservable(options.fade) : true, //default is to fade it out in presence of jquery
+            callback = options.callback !== undefined ? ko.utils.unwrapObservable(options.callback) : function() {},
 			jQueryExists = typeof jQuery != 'undefined';
 		
 		//set the element's text to the value of the message
@@ -41,14 +42,22 @@ ko.bindingHandlers.notification = {
 				//if jQuery is there, run the fadeOut, otherwise do old-timey js
 				if (jQueryExists) {
 					if (fade)
-						jQuery(element).fadeOut(fadeoutDuration, function() {options.message('')});
+						jQuery(element).fadeOut(fadeoutDuration, function() {
+                            options.message('');
+                            callback();
+                        });
 					else {
 						jQuery(element).hide();
 						options.message('');
+                        callback();
 					}
-				} else
+				} else {
 					element.style.display = 'none';
+                    callback();
+				}
 			}, duration);
+		} else {
+            callback();
 		}
 	}
 };
